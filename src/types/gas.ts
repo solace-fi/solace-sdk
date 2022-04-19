c846f97e4e0e96c3b14970756a2e0f6b3abae460
Danny Fung <dannyfung97@gmail.com>
2022-04-18 18:46:33 -0700

    refactor: updated gas function

--- src/types/gas.ts
+++ src/types/gas.ts
@@ -1,12 +1,21 @@
+import { BigNumber } from "ethers"
+
 export type GasConfiguration ={
   gasPrice?: number
   maxFeePerGas?: number
   maxPriorityFeePerGas?: number
   type?: number
+  gasLimit?: number
 }
 
 export type GasArgs = {
   connector?: string
   gasLimit?: number
   gasForTestnet?: boolean
+}
+
+export interface FeeData {
+  maxFeePerGas: null | BigNumber;
+  maxPriorityFeePerGas: null | BigNumber;
+  gasPrice: null | BigNumber;
 }

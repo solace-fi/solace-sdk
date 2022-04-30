505fa3cbbeac801d53dc7075625a2a64b374a19f
Danny Fung <dannyfung97@gmail.com>
2022-04-29 19:10:14 -0700

    chore: removed getSigner

--- src/utils/ethers.ts
+++ src/utils/ethers.ts
@@ -1,12 +1,3 @@
 import { providers } from 'ethers'
-import { WALLETS } from '..'
 
 export const getProvider = (rpcUrl: string): providers.JsonRpcProvider => new providers.JsonRpcProvider(rpcUrl)
-
-export const getSigner = async (account?: string, walletId?: string): Promise<providers.JsonRpcSigner> => {
-  const foundWallet = walletId ? WALLETS.find((w) => w.id.toLowerCase() == walletId) ?? WALLETS[0] : WALLETS[0]
-  const { provider } = await foundWallet.connector.activate();
-  const ethProvider = new providers.Web3Provider(provider, "any");
-  const signer = account ? ethProvider.getSigner(account) : ethProvider.getSigner()
-  return signer
-}
